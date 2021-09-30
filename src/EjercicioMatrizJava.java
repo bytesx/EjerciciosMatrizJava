36e560e2aa13eaba97c82fde6c808c7e95ed66cc
bytesx <pnada.black.edg@gmail.com>
2021-09-30 02:19:16 -0500

    Adding Information and obejectives

---
+++ src/EjercicioMatrizJava.java
@@ -0,0 +1,19 @@
+public class EjercicioMatrizJava {
+    public static void main(String[] args) {
+        //Edgar Vazquez - Java - Ejercicio con Matriz
+        // Objetivos del Ejercicio
+        // 1.- Crear Matriz 5 x5
+        // 2.- Llenar Matriz con numeros Aleatorios
+        // 3.- Mostrar promedio de la matriz
+        // 4.- Mostrar numero mayor y cuanto se repite
+        // 5.- Mostrar en consola todos los números primos.
+        // 6.- Mostrar en consola todos los números pares.
+        // 7.- Mostrar en consola la suma de las diagonales
+        // 8.- Mostrar en consola la suma de la la última fila.
+        // 9.- Mostrar en consola el mensaje "Cuadrado Mágico" si la matriz cataloga como un cuadrado mágico.
+
+        //Declarando Variables
+        int asuMatriz [][][][][][];
+
+    }
+}
